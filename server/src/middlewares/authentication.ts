5404f7efa1872e5d893c1505a661c8507bc36966
kris.nguyen <kris.nguyen@avepoint.com>
2024-07-26 11:47:26 +0700

    Upadate webhooks

--- server/src/middlewares/authentication.ts
+++ server/src/middlewares/authentication.ts
@@ -6,34 +6,34 @@ import { commonRes } from "../helper";
 const withoutCredentialRoutes = ["/auth", "/api/webhooks"];
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const pathName = req.url;
-  if (withoutCredentialRoutes.some((item) => pathName.startsWith(item))) {
-    next();
-    return;
-  }
+  // const pathName = req.url;
+  // if (withoutCredentialRoutes.some((item) => pathName.startsWith(item))) {
+  //   next();
+  //   return;
+  // }
 
-  const credential = req.headers.authorization;
+  // const credential = req.headers.authorization;
 
-  if (credential?.startsWith("Bearer ")) {
-    try {
-      const token = credential.substring(7, credential.length);
-      const decoded = jwtDecode(token);
-      console.log(decoded);
+  // if (credential?.startsWith("Bearer ")) {
+  //   try {
+  //     const token = credential.substring(7, credential.length);
+  //     const decoded = jwtDecode(token);
+  //     console.log(decoded);
 
-      const isExpired = Date.now() >= decoded?.exp * 1000;
-      if (isExpired) return commonRes(res, { status: 405, message: "Unauthorized" });
+  //     const isExpired = Date.now() >= decoded?.exp * 1000;
+  //     if (isExpired) return commonRes(res, { status: 405, message: "Unauthorized" });
 
-      const email: string = (decoded as any)?.email;
-      const currentUser = await getUserByEmail(email);
+  //     const email: string = (decoded as any)?.email;
+  //     const currentUser = await getUserByEmail(email);
 
-      if (!currentUser) return commonRes(res, { status: 405, message: "Token Invalid" });
-    } catch (e) {
-      console.log(e);
-      return commonRes(res, { status: 405, message: "Token Invalid" });
-    }
-  } else {
-    return commonRes(res, { status: 405, message: "Token Invalid" });
-  }
+  //     if (!currentUser) return commonRes(res, { status: 405, message: "Token Invalid" });
+  //   } catch (e) {
+  //     console.log(e);
+  //     return commonRes(res, { status: 405, message: "Token Invalid" });
+  //   }
+  // } else {
+  //   return commonRes(res, { status: 405, message: "Token Invalid" });
+  // }
 
   next();
 };
